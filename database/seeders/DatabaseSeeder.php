f0cffecd3a1c687aaf8c6ad055b409c52df928a3
Alejandro Castrillón <4lejo.castrillon@gmail.com>
2022-02-15 16:47:45 -0500

    Edit the db seeder file
    Extract movies data from movies.json and insert it in database.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,16 +3,38 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Movie;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // \App\Models\User::factory(10)->create();
-    }
+	private $movies;
+	/**
+	 * Seed the application's database.
+	 *
+	 * @return void
+	 */
+	public function run()
+	{
+		// \App\Models\User::factory(10)->create();
+		self::seedCatalog();
+		$this->command->info('Tabla catálogo inicializada con datos!');
+	}
+
+	public function seedCatalog()
+	{
+		$this->	movies = json_decode(file_get_contents(storage_path() . "/movies.json"), true)['movies'];
+		DB::table('movies')->delete();
+		foreach ($this->movies as $movie) {
+			$newMovie = new Movie;
+
+			$newMovie->title = $movie['title'];
+			$newMovie->year = $movie['year'];
+			$newMovie->director = $movie['director'];
+			$newMovie->poster = $movie['poster'];
+			$newMovie->rented = $movie['rented'];
+			$newMovie->synopsis = $movie['synopsis'];
+			$newMovie->save();
+		}
+	}
 }
